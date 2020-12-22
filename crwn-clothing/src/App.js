1ba86ed3fba9d072ca45553e9ca8c3c292ca43d2
haarris <haarris82@gmail.com>
2020-12-22 21:13:44 +1100

    crwn clothing first

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.component';
+
+/*CRWN CLOTHING PLANNING:
+  - top has a header
+  - Has one big container (directory-menu) 
+    holding 3 small and two medium (total 5, menu-item)
+    components
+  - Each component has a container holding the image and inside
+    component holding the title
+
+*/
+function App() {
+  return (
+    <div>
+      <HomePage />     
+    </div>
+  );
+}
+
+export default App;
