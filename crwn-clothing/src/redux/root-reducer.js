0284a612253be49cdc202db9e82306e726a5175d
haarris <haarris82@gmail.com>
2021-04-19 14:54:07 +1000

    cart icon and cart drop down

--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -3,7 +3,9 @@ import { useReducer } from 'react';
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
